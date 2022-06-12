8468ac7f929b06c637b0c80fe3035266445c1b53
Ayl <laprognet@gmail.com>
2022-06-12 22:01:06 +0500

    Fix 7 card hand evaluator

--- src/main/scala/nurekata/Hand.scala
+++ src/main/scala/nurekata/Hand.scala
@@ -30,35 +30,47 @@ object Hand:
       val ranks = cs.map(c => c.rank).sortedDesc
       flush(cs)
          .map(rs =>
-            straight(rs.sortedDesc)
-               .map(Hand(StraightFlush, _))
+            straight(rs)
+               .map(r => Hand(StraightFlush, List(r)))
                .getOrElse(Hand(Flush, rs.take(5)))
          )
          .orElse(
             straight(ranks)
-               .map(Hand(Straight, _))
+               .map(r => Hand(Straight, List(r)))
          )
          .getOrElse(repeats(ranks))
 
    def repeats(sorted: List[Rank]): Hand =
       val (os, rs) = occurs(sorted).sortedDesc.unzip
       os match
-         case 4 :: _      => Hand(Quads, rs.take(2))
+         case 4 :: _      => Hand(Quads, List(rs.head, rs.tail.max))
          case 3 :: 2 :: _ => Hand(FullHouse, rs.take(2))
-         case 3 :: _      => Hand(Trips, rs.take(2))
-         case 2 :: 2 :: _ => Hand(TwoPair, rs.take(3))
-         case 2 :: _      => Hand(Pair, rs.take(2))
+         case 3 :: _      => Hand(Trips, rs.take(3))
+         case 2 :: 2 :: _ => Hand(TwoPair, rs.take(2) :+ rs.drop(2).max)
+         case 2 :: _      => Hand(Pair, rs.take(4))
          case _           => Hand(HighCard, sorted.take(5))
 
    def flush(cs: List[Card]): Option[List[Rank]] =
       cs.groupBy(_.suit)
          .values
-         .collectFirst { case cs if cs.size >= 5 => cs.map(_.rank) }
+         .collectFirst { case cs if cs.size >= 5 => cs.map(_.rank).sortedDesc }
 
-   def straight(sorted: List[Rank]): Option[List[Rank]] =
-      sorted
-         .sliding(5)
-         .find(Hand5.isStraight)
+   def straight(sorted: List[Rank]): Option[Rank] =
+      def lowStraight(rs: List[Rank]): Option[Rank] =
+         Option.when(
+            rs.head == Ace &&
+               rs.endsWith(List(Five, Four, Three, Two))
+         )(Five)
+
+      val distinct =
+         sorted
+            .zip(sorted.tail)
+            .collect { case (h, l) if h != l => h }
+
+      distinct.view
+         .zip(distinct.drop(4))
+         .collectFirst { case (h, l) if h.value == l.value + 4 => h }
+         .orElse(lowStraight(distinct))
 
    def occurs(sorted: List[Rank]): List[(Int, Rank)] =
       sorted match
